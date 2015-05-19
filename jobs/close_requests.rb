35e691db08c9c89f7ec27c2e74f86b5f53d40ed0
KJsharmila <sharmila437@gmail.com>
2015-05-19 10:50:26 +0530

    open pr

--- jobs/close_requests.rb
+++ jobs/close_requests.rb
@@ -1,14 +1,14 @@
 require 'octokit'
 require 'time'
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  client = Octokit::Client.new(:access_token => "d47e6387d6ddc4437707eaa439643a8460f91f69")
+SCHEDULER.every '10s', :first_in => 0 do |job|
+  client = Octokit::Client.new(:access_token => "60071a13e9d7f0816c08149b5435dd8eac26cc3b")
   my_organization = "Qwinix"
   repo_name = []
   client.organization_repositories(my_organization).map do |repo| 
     repo_name << repo.name if repo.name == 'loan_list'
   end
 
-  open_pull_requests = repo_name.inject([]) { |pulls, repo|
+  close_pull_requests = repo_name.inject([]) { |pulls, repo|
     client.pull_requests("#{my_organization}/#{repo}", :state => 'close').each do |pull|
       pulls.push({
         title: pull.title,
@@ -19,5 +19,5 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     end
     pulls[0..3]
   }
-  send_event('closedPrs', { header: "Close Pull Requests", pulls: open_pull_requests })
+  send_event('closedPrs', { header: "Close Pull Requests", pulls: close_pull_requests })
 end
--- jobs/pull_requests.rb
+++ jobs/pull_requests.rb
@@ -1,16 +1,15 @@
 require 'octokit'
-
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  client = Octokit::Client.new(:access_token => "d47e6387d6ddc4437707eaa439643a8460f91f69")
+require 'time'
+SCHEDULER.every '10s', :first_in => 0 do |job|
+  client = Octokit::Client.new(:access_token => "60071a13e9d7f0816c08149b5435dd8eac26cc3b")
   my_organization = "Qwinix"
   repo_name = []
   client.organization_repositories(my_organization).map do |repo| 
     repo_name << repo.name if repo.name == 'loan_list'
   end
-  repos = client.organization_repositories(my_organization).map { |repo| repo.name }
 
-  open_pull_requests = repos.inject([]) { |pulls, repo|
-    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
+  open_pull_requests = repo_name.inject([]) { |pulls, repo|
+    client.pull_requests("#{my_organization}/#{repo}", :state => 'close').each do |pull|
       pulls.push({
         title: pull.title,
         repo: repo,
@@ -18,7 +17,7 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
         creator: "@" + pull.user.login,
         })
     end
-    pulls
+    pulls[0..3]
   }
   send_event('openPrs', { header: "Open Pull Requests", pulls: open_pull_requests })
 end
