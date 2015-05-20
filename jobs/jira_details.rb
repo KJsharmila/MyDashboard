e9ee7f5e929c9d29a4407733faae8ccc2fe0e418
KJsharmila <sharmila437@gmail.com>
2015-05-20 17:51:44 +0530

    open

--- jobs/jira_details.rb
+++ jobs/jira_details.rb
@@ -60,5 +60,5 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
   end
   
   total = todo_count + open_count + reopened_count + in_progress_count + done_count + dev_done_count + qa_count + uat_count + resolved_count
-  send_event("jira", { title: "Jira Story Details", todo: todo_count, open: open_count, reopened: reopened_count, inprogress: in_progress_count, qa: qa_count, uat: uat_count, dev_done: dev_done_count, resolved:resolved_count, done: done_count, total: total })
+  send_event("jira", { title: "JIRA STORY DETAILS", todo: todo_count, open: open_count, reopened: reopened_count, inprogress: in_progress_count, qa: qa_count, uat: uat_count, dev_done: dev_done_count, resolved:resolved_count, done: done_count, total: total })
 end
--- jobs/pull_requests.rb
+++ jobs/pull_requests.rb
@@ -1,7 +1,6 @@
 require 'octokit'
-require 'time'
 SCHEDULER.every '10s', :first_in => 0 do |job|
-  client = Octokit::Client.new(:access_token => "74c32e32583c9ade814d7755fcc0aa7334277a18")
+  client = Octokit::Client.new(:access_token => "371831bf66d14bf8743862cc68518d4090c5c85a")
   my_organization = "Qwinix"
   repo_name = []
   client.organization_repositories(my_organization).map do |repo| 
@@ -9,7 +8,7 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
   end
 
   open_pull_requests = repo_name.inject([]) { |pulls, repo|
-    client.pull_requests("#{my_organization}/#{repo}", :state => 'close').each do |pull|
+    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
       pulls.push({
         title: pull.title,
         repo: repo,
@@ -17,7 +16,7 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
         creator: "@" + pull.user.login,
         })
     end
-    pulls[0..0]
+    pulls
   }
   send_event('openPrs', { header: "Open Pull Requests", pulls: open_pull_requests })
 end
