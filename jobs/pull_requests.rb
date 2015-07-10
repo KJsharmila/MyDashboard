a4c0719a13608f038641e454c82fbf1efbb9c2f6
hujwala <upatel@qwinixtech.com>
2015-07-10 12:13:43 +0530

    access token

--- jobs/pull_requests.rb
+++ jobs/pull_requests.rb
@@ -2,7 +2,7 @@ require 'octokit'
 require 'time'
 SCHEDULER.every '10s', :first_in => 0 do |job|
 
-  client = Octokit::Client.new(:access_token => "c7f5649dffba2fe5b2483f473e7381bf3263b6ba")
+  client = Octokit::Client.new(:access_token => "fdd72adcd67f72e10013b6441c6a0cb78b9f4ac7")
   my_organization = "Qwinix"
   repo_name = []
 
