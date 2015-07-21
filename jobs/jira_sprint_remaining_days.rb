6a0b1186b8deecfad76bc6b6bc32dc1369a67147
hujwala <upatel@qwinixtech.com>
2015-07-21 09:42:57 +0530

    fdf

--- jobs/jira_sprint_remaining_days.rb
+++ jobs/jira_sprint_remaining_days.rb
@@ -1,10 +1,10 @@
-# Displays the board name, sprint name and remaining days for the active sprint for a specific board in Jira Agile
-require 'net/http'
-require 'json'
-require 'time'
+# # Displays the board name, sprint name and remaining days for the active sprint for a specific board in Jira Agile
+# require 'net/http'
+# require 'json'
+# require 'time'
 
-# JIRA_URI = URI.parse("https://qwinix.atlassian.net")
-JIRA_AUTH = {
-	'name' => 'bmsantosh',
-	'password' => 'Qwinix@123'
-}
+# # JIRA_URI = URI.parse("https://qwinix.atlassian.net")
+# JIRA_AUTH = {
+# 	'name' => 'bmsantosh',
+# 	'password' => 'Qwinix@123'
+# }
